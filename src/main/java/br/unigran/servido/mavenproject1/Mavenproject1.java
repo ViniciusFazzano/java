ef8acfcfc9f690d74580b5fc650f0069874f84c2
laboratorio <laboratorio@LAB52-41.lab.unigran>
2024-04-25 19:18:32 -0400

    teste

---
+++ src/main/java/br/unigran/servido/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unigran.servido.mavenproject1;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

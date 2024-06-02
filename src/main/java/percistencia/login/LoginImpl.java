2badfb72e06bd91d2ef92f89a7da51788fb31c56
Vinicius Da Gama Fazzano <83099381+ViniciusFazzano@users.noreply.github.com>
2024-06-02 17:40:41 -0300

    Update LoginImpl.java

--- src/main/java/percistencia/login/LoginImpl.java
+++ src/main/java/percistencia/login/LoginImpl.java
@@ -6,6 +6,8 @@ package percistencia.login;
 
 import entidades.Login;
 import java.util.List;
+import javax.persistence.PersistenceException;
+import org.json.JSONObject;
 import percistencia.padrao.Dao;
 
 /**
@@ -16,7 +18,27 @@ public class LoginImpl implements LoginDao {
 
     @Override
     public List<Login> listar() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            return Dao.getInstace().getEm().createQuery("SELECT l FROM Login l").getResultList();
+        } catch (PersistenceException e) {
+            System.err.println("Erro ao listar usuários: " + e.getMessage());
+            return null; // Ou lançar a exceção novamente após tratamento ou retornar uma lista vazia
+        }
+    }
+
+    public boolean verificaExiste(JSONObject entidade) {
+        try {
+            Long count = (Long) Dao.getInstace().getEm()
+                    .createQuery("SELECT COUNT(l) FROM Login l WHERE l.usuario = :usuario AND  l.senha = :senha AND  l.funcao = :funcao")
+                    .setParameter("usuario", entidade.get("nomelogin").toString())
+                    .setParameter("senha", entidade.get("pass").toString())
+                    .setParameter("funcao", entidade.get("tipoUsuario"))
+                    .getSingleResult();
+            return count > 0;
+        } catch (Exception e) {
+            System.err.println("Erro ao verificar existência: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
